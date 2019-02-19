602e9b2a9a4ad72e08bb73af826794e0d4b5097a
and-gorbik <and-gorbik@mail.ru>
2019-02-19 22:48:59 +0300

    create non-optimized solution, which still doesn't work

--- src/map.py
+++ src/map.py
@@ -28,129 +28,88 @@ class MapContainer:
     def is_empty(self):
         return len(self._placed_items) == 0
 
+    def unplaced_items(self):
+        return self._items
+
 
 class Map:
     def __init__(self, items, size=2):
-        self._resize_map(size)
-        self._curi, self._curj = 0, 0
         self._container = MapContainer(items)
+        self._resize_map(size)
+        # self._reset_position()
 
     def __str__(self):
         return "\n".join(["".join(line) for line in self._data])
 
     def fill(self):
-        while not self._container.is_full():
-            while self._next_position():
-                if self._check_position():
-                    self._place_item()
-                elif not self._container.is_empty():
+        while  not self._container.is_full():
+            if not self._place_item():
+                if self._container.is_empty():
+                    self._resize_map(self.size + 1)
+                else:
                     self._remove_item()
-            self._resize_map(self.size + 1)
-            self._curi, self._curj = 0, 0
 
     def _resize_map(self, size):
         self.size = size
-        self._data = [['.'] * size] * size
+        self._data = [['.' for _ in range(size)] for _ in range(size)]
+        for i in self._container.unplaced_items():
+            i.set_position(0, 0)
 
-    def _next_position(self):
-        if self._curj + 1 < self.size:
-            self._curj += 1
-            return True
-        if self._curi + 1 < self.size:
-            self._curi += 1
-            self._curj = 0
-            return True
-        return False
 
-    def _check_position(self):
+    def _check_position(self, curi, curj):
         for i, j in self._container.next().points:
-            if (i + self._curi >= self.size) or (j + self._curj >= self.size):
+            if (i + curi >= self.size) or (j + curj >= self.size):
                 return False
-            if self._data[i + self._curi][j + self._curj] not in '.':
+            if self._data[i + curi][j + curj] not in '.':
                 return False
         return True
 
     def _place_item(self):
-        item = self._container.push()
-        for i, j in item.points:
-            self._data[i + self._curi][j + self._curj] = item.chr
-
-    def _remove_item(self):
-        item = self._container.pop()
-        self._curi, self._curj = item.get_position()
-        item.set_position(0, 0)
-        for i, j in item.points:
-            i, j = item.get_position()
-            self._data[i][j] = '.'
-
-
-"""
-class Map:
-    def __init__(self, size=2):
-        self._resize_map(size)
-        self._curi, self._curj = 0, 0
-        self._placed_items = []
-
-    def __str__(self):
-        return "\n".join(["".join(line) for line in self._data])
-
-    def _resize_map(self, size):
-        self.size = size
-        self._data = [['.'] * size] * size
-
-    def _check_points(self, item, di, dj):
-        for i, j in item.points:
-            if (i + di >= self.size) or (j + dj >= self.size):
-                return False
-            if self._data[i + di][j + dj] not in '.':
-                return False
-        return True
-
-    def _add_item(self, item):
-        for di in range(self._curi, self.size):
-            for dj in range(self._curj, self.size):
-                if self._check_points(item, di, dj):
-                    item.posi, item.posj = di, dj
-                    self._placed_items.append(item)
+        curi, curj = self._container.next().get_position()
+        while curi < self.size:
+            while curj < self.size:
+                if self._check_position(curi, curj):
+                    item = self._container.push()
                     for i, j in item.points:
-                        self._data[i + di][j + dj] = item.chr
+                        self._data[i + curi][j + curj] = item.chr
+                    item.set_position(curi, curj)
+                    print(self, '\n')
                     return True
         return False
 
-    def _remove_prev_item(self):
-        item = self._placed_items.pop()
+    def _remove_item(self):
+        item = self._container.pop()
+        curi, curj = item.get_position()
         for i, j in item.points:
-            self._data[i + item.posi][j + item.posj] = '.'
-        return item
+            self._data[i + curi][j + curj] = '.'
+        item.set_position(curi + 1, curj + 1)
 
-    def _move_position(self):
-        if j + 1 < self.size:
-            self._curj += 1
-            return True
-        if i + 1 < self.size:
-            self._curi += 1
-            self._curj = 0
-            return True
-        return False
+    # def _next_position(self):
+    #     if self._curj + 1 < self.size:
+    #         self._curj += 1
+    #         return True
+    #     if self._curi + 1 < self.size:
+    #         self._curi += 1
+    #         self._curj = 0
+    #         return True
+    #     return False
 
-    def fill(self, figures):
-        while len(figures):
-            # print("Stack size: {} - CurI: {} - CurJ: {}".format(len(figures), self._curi, self._curj))
-            if self._add_item(figures[0]):
-                self._curi, self._curj = figures[0].posi, figures[0].posj
-                figures.pop(0)
-                print(self, "\n")
-            # elif size == len(figures):
-            #     self._resize_map(self.size + 1)
-            #     self._curi, self._curj = 0, 0
-            else:
-                if len(self._placed_items) != 0: # если есть хотя бы один размещенный элемент
-                    item = self._remove_prev_item()
-                    self._curi, self._curj = item.posi, item.posj
-                    item.posi, item.posj = 0, 0
-                    figures.insert(0, item)
-                else
-"""
+    # def __next__(self):
+    #     if self._curj + 1 < self.size:
+    #         self._curj += 1
+    #         return self._curi, self._curj
+    #     if self._curi + 1 < self.size:
+    #         self._curi += 1
+    #         self._curj = 0
+    #         return self._curi, self._curj
+    #     raise StopIteration
+
+    # def __iter__(self):
+    #     return self
+
+    # def _reset_position(self):
+    #     self._curi, self._curj = 0, -1
+
 
 if __name__ == '__main__':
     A = "#...\n#...\n#...\n#...\n"
