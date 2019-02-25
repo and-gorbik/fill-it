3eeb0bf556950c1c31b53708568e26702a6c2744
and-gorbik <and-gorbik@mail.ru>
2019-02-25 16:52:45 +0300

    finish optimizations

--- src/map.py
+++ src/map.py
@@ -66,7 +66,7 @@ class Map:
         self._data = [['.' for _ in range(size)] for _ in range(size)]
         for i in self._container.unplaced_items():
             i.set_position(0, 0)
-
+        self._optimizer = Optimizer(size=size)
 
     def _check_position(self, curi, curj):
         for i, j in self._container.next().points:
@@ -79,15 +79,18 @@ class Map:
     def _place_item(self):
         curi, curj = self._container.next().get_position()
         while curi < self.size:
-            while curj < self.size:
-                if self._check_position(curi, curj):
-                    item = self._container.push()
-                    for i, j in item.points:
-                        self._data[i + curi][j + curj] = item.chr
-                    item.set_position(curi, curj)
-                    print(self, '\n')
-                    return True
-                curj += 1
+            if self._optimizer.is_available_row(curi):
+                while curj < self.size:
+                    if self._optimizer.is_available_col(curj) and self._check_position(curi, curj):
+                        item = self._container.push()
+                        for i, j in item.points:
+                            self._data[i + curi][j + curj] = item.chr
+                        item.set_position(curi, curj)
+                        self._optimizer.set_item(item)
+                        print(self, '\n')
+                        print(self._optimizer)
+                        return True
+                    curj += 1
             curi += 1
             curj = 0
         self._container.next().set_position(0, 0)
@@ -98,6 +101,7 @@ class Map:
         curi, curj = item.get_position()
         for i, j in item.points:
             self._data[i + curi][j + curj] = '.'
+        self._optimizer.unset_item(item)
         if curj + 1 < self.size:
             item.set_position(curi, curj + 1)
         elif curi + 1 < self.size:
@@ -105,30 +109,12 @@ class Map:
 
 
 class Optimizer:
-    def __init__(self):
-        self._vector_i = []
-        self._vector_j = []
-        self._table = []
-        self._size = 0
-
-    def _calculate_cell(self, i, j):
-        result = 0
-        if j - 1 >= 0 and self._table[i][j - 1] != 0:
-            result += 1
-        if j + 1 < self._size and self._table[i][j + 1] != 0:
-            result += 1
-        if i - 1 >= 0 and self._table[i - 1][j] != 0:
-            result += 1
-        if i + 1 < self._size and self._table[i + 1][j] != 0:
-            result += 1
-        self._table[i][j] = result
-
-    def init_map(self, size):
+    def __init__(self, size):
         if size < 2:
             print("Size must be >= 2!", file=stderr)
             size = 2
         self._size = size
-        self._vector_i = [4 for _ in range(size)]
+        self._vector_i = [size for _ in range(size)]
         self._vector_j = self._vector_i[:]
         self._table = [[4 for _ in range(size)] for _ in range(size)]
         for i in range(size):
@@ -138,48 +124,116 @@ class Optimizer:
                     if (i == 0 or i == size - 1) and (j == 0 or j == size - 1):
                         self._table[i][j] = 2
                 else:
-                    self._table[i][j] = 4
+                    self._table[i][j] = 4  
+
+    def _calculate_cell(self, i, j):
+        result = 0
+        if j - 1 >= 0 and self._table[i][j - 1] != -1:
+            result += 1
+        if j + 1 < self._size and self._table[i][j + 1] != -1:
+            result += 1
+        if i - 1 >= 0 and self._table[i - 1][j] != -1:
+            result += 1
+        if i + 1 < self._size and self._table[i + 1][j] != -1:
+            result += 1
+        self._table[i][j] = result
+
+    def _decrement_neighbour_cells(self, i, j):
+        if j - 1 >= 0 and self._table[i][j - 1] != -1:
+            self._table[i][j - 1] -= 1
+        if j + 1 < self._size and self._table[i][j + 1] != -1:
+            self._table[i][j + 1] -= 1
+        if i - 1 >= 0 and self._table[i - 1][j] != -1:
+            self._table[i - 1][j] -= 1         
+        if i + 1 < self._size and self._table[i + 1][j] != -1:
+            self._table[i + 1][j] -= 1
+
+    def _increment_neighbour_cells(self, i, j):
+        if j - 1 >= 0 and self._table[i][j - 1] != -1 and self._table[i][j - 1] != 5:
+            self._table[i][j - 1] += 1
+        if j + 1 < self._size and self._table[i][j + 1] != -1 and self._table[i][j + 1] != 5:
+            self._table[i][j + 1] += 1
+        if i - 1 >= 0 and self._table[i - 1][j] != -1 and self._table[i - 1][j] != 5:
+            self._table[i - 1][j] += 1
+        if i + 1 < self._size and self._table[i + 1][j] != -1 and self._table[i + 1][j] != 5:
+            self._table[i + 1][j] += 1
 
     def set_item(self, item: Item):
         curi, curj = item.get_position()
         for i, j in item.points:
-            pass
+            self._table[i + curi][j + curj] = -1
+            self._vector_i[i + curi] -= 1
+            self._vector_j[j + curj] -= 1
+
+        # для каждой клетки проверить 4 смежные;
+        # там, где не -1, уменьшить значение на 1
+        for i, j in item.points:
+            self._decrement_neighbour_cells(i + curi, j + curj)
 
     def unset_item(self, item):
-        pass
+        curi, curj = item.get_position()
+        # значение каждой клетки установить в 5,
+        # чтобы исключить эти клетки из дальнейшей проверки
+        for i, j in item.points:
+            self._table[i + curi][j + curj] = 5
+            self._vector_i[i + curi] += 1
+            self._vector_j[j + curj] += 1    
+
+        # для каждой клетки проверить 4 смежные;
+        # там, где значение != -1 и != 5, увеличить значение на 1
+        for i, j in item.points:
+            self._increment_neighbour_cells(i + curi, j + curj)
+        
+        # установить значение каждой клетки
+        for i, j in item.points:
+            self._calculate_cell(i + curi, j + curj)
 
     def is_available_row(self, i):
         """ позволяет узнать, нужно ли проверять строку """
+        if not self._vector_i[i]:
+            return False
+        # TODO: дописать условия, при которых также return False
+        return True
 
     def is_available_col(self, j):
         """ позволяет узнать, нужно ли проверять столбец """
+        if not self._vector_j[j]:
+            return False
+        # TODO: дописать условия, при которых также return False
+        return True
 
     def __str__(self):
-        return "\n".join(["".join(str(line)) for line in self._table])
+        #return "\n".join(["".join(str(line)) for line in self._table])
+        result = []
+        for i in range(self._size):
+            for j in range(self._size):
+                if self._table[i][j] == -1:
+                    result.append(" ")
+                else:
+                    result.append(str(self._table[i][j]))
+            result.append("\n")
+        return "".join(result)
+
         
 
 if __name__ == '__main__':
-    # A = "#...\n#...\n#...\n#...\n"
-    # B = "####\n....\n....\n....\n"
-    # C = "###.\n..#.\n....\n....\n"
-    # D = ".##.\n##..\n....\n....\n"
-    # E = "##..\n##..\n....\n....\n"
-    # F = "##..\n.##.\n....\n....\n"
-    # G = "##..\n.#..\n.#..\n.#..\n"
-    # H = "###.\n.#..\n....\n....\n"
-    # a = Item(A, 'A')
-    # b = Item(B, 'B')
-    # c = Item(C, 'C')
-    # d = Item(D, 'D')
-    # e = Item(E, 'E')
-    # f = Item(F, 'F')
-    # g = Item(G, 'G')
-    # h = Item(H, 'H')
-    # m = Map([a, b, c, d, e, f, g, h])
-    # with extimer():
-    #     m.fill()
-    # print(m)
-    o = Optimizer()
-    o.init_map(5)
-    o._calculate_cell(4, 4)
-    print(o)
+    A = "#...\n#...\n#...\n#...\n"
+    B = "####\n....\n....\n....\n"
+    C = "###.\n..#.\n....\n....\n"
+    D = ".##.\n##..\n....\n....\n"
+    E = "##..\n##..\n....\n....\n"
+    F = "##..\n.##.\n....\n....\n"
+    G = "##..\n.#..\n.#..\n.#..\n"
+    H = "###.\n.#..\n....\n....\n"
+    a = Item(A, 'A')
+    b = Item(B, 'B')
+    c = Item(C, 'C')
+    d = Item(D, 'D')
+    e = Item(E, 'E')
+    f = Item(F, 'F')
+    g = Item(G, 'G')
+    h = Item(H, 'H')
+    m = Map([a, b, c, d, e, f, g, h])
+    with extimer():
+        m.fill()
+    print(m)
