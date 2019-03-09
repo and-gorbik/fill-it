fe2ae7091212fab21e8fd1181dce7d8e6a8ca818
and-gorbik <and-gorbik@mail.ru>
2019-03-09 14:30:57 +0300

    validation works

--- src/C/item.c
+++ src/C/item.c
@@ -1,139 +1,149 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   item.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sjacelyn <sjacelyn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/09 09:38:02 by sjacelyn          #+#    #+#             */
+/*   Updated: 2019/03/09 14:20:37 by sjacelyn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "item.h"
 
-/* TODO: add to libft */
-static int  ft_strcount(const char *s, char chr)
+static int	ft_strcount(const char *s, char chr)
 {
-    int counter;
+	int	counter;
 
-    counter = 0;
-    while (*s)
-    {
-        if (*s == chr)
-            ++counter;
-        ++s;
-    }
-    return (counter);
+	counter = 0;
+	while (*s)
+	{
+		if (*s == chr)
+			++counter;
+		++s;
+	}
+	return (counter);
 }
 
-static char **_to_matrix(const char *s)
+static char		**to_matrix(const char *s)
 {
-    char    **matrix;
-    int     i;
+	char	**matrix;
+	int		i;
 
-    if (!(matrix = ft_strsplit(s, '\n')))
-        return (NULL);
-    i = 0;
-    while (matrix[i])
-    {
-        if (ft_strlen(matrix[i]) != 4)
-        {
-            while (*matrix)
-                free(*matrix++);
-            free(matrix);
-            return (NULL);
-        }
-        ++i;
-    }
-    return (matrix);
+	if (!(matrix = ft_strsplit(s, '\n')))
+		return (NULL);
+	i = 0;
+	while (matrix[i])
+	{
+		if (ft_strlen(matrix[i]) != 4)
+		{
+			i = -1;
+			while (matrix[++i])
+				free(matrix[i]);
+			free(matrix);
+			return (NULL);
+		}
+		++i;
+	}
+	return (matrix);
 }
 
-t_point     _get_offset(char **matrix)
+static t_point	get_offset(char **matrix)
 {
-    t_point offset;
-    int     i;
-    char    *ptr;
+	t_point	offset;
+	int		i;
+	char	*ptr;
 
-    offset.i = 0;
-    offset.j = 4;
-    i = 0;
-    while (matrix[i])
-    {
-        ptr = ft_strchr(matrix[i], '#');
-        if (ptr - matrix[i] < offset.j)
-            offset.j = ptr - matrix[i];
-        if (!ptr)
-            offset.i += 1;
-        ++i;
-    }
-    return (offset);
+	offset.j = 4;
+	i = 0;
+	while (matrix[i])
+	{
+		ptr = ft_strchr(matrix[i], '#');
+		if (ptr && ptr - matrix[i] < offset.j)
+			offset.j = ptr - matrix[i];
+		++i;
+	}
+	offset.i = 0;
+	i = 0;
+	while (matrix[i] && !ft_strchr(matrix[i], '#'))
+	{
+		offset.i = i;
+		++i;
+	}
+	return (offset);
 }
 
-static void _set_points(t_item *item, char **matrix)
+static void		set_points(t_item *item, char **matrix)
 {
-    t_point offset;
-    int     i;
-    int     k;
+	t_point	offset;
+	int		i;
+	int		k;
 
-    if (!matrix)
-    {
-        item->is_valid = 0;
-        return ;
-    }
-    offset = _get_offset(matrix);
-    k = 0;
-    i = 0;
-    while (matrix[i / 4][i % 4])
-    {
-        if (matrix[i / 4][i % 4] == '#')
-        {
-            item->points[k].i = i / 4 - offset.i;
-            item->points[k].j = i % 4 - offset.j;
-            ++k;
-        }
-        ++i;
-    }
-    while (*matrix)
-        free(*matrix++);
-    free(matrix);
+	if (!matrix)
+	{
+		item->is_valid = 0;
+		return ;
+	}
+	offset = get_offset(matrix);
+	k = 0;
+	i = 0;
+	while (i < 16)
+	{
+		if (matrix[i / 4][i % 4] == '#')
+		{
+			item->points[k].i = i / 4 - offset.i;
+			item->points[k].j = i % 4 - offset.j;
+			++k;
+		}
+		++i;
+	}
+	while (--k >= 0)
+		free(matrix[k]);
+	free(matrix);
 }
 
-static void _validate_item(t_item *item)
+static void		validate_item(t_item *item)
 {
-    int     counter;
-    int     i;
-    int     j;
+	t_point	cur;
+	t_point	other;
+	int		c;
+	int		i;
+	int		j;
 
-    if (!item->is_valid)
-        return;
-    counter = 0;
-    i = 0;
-    while (i < 4)
-    {
-        t_point cur = item->points[i];
-        j = 0;
-        while (j < 4)
-        {
-            t_point other = item->points[j];
-            counter += (cur.j - 1 >= 0 && other.i == cur.i && other.j == cur.j - 1);
-            counter += (cur.i - 1 >= 0 && other.i == cur.i - 1 && other.j == cur.j);
-            counter += (cur.j + 1 < 4 && other.i == cur.i && other.j == cur.j + 1);
-            counter += (cur.i + 1 < 4 && other.i == cur.i + 1 && other.j == cur.j);
-            ++j;
-        }
-        ++i;
-    }
-    if (counter < 6)
-        item->is_valid = 0;
+	if (!item->is_valid)
+		return ;
+	c = 0;
+	i = -1;
+	while (++i < 4)
+	{
+		cur = item->points[i];
+		j = -1;
+		while (++j < 4)
+		{
+			other = item->points[j];
+			c += (cur.j - 1 >= 0 && other.i == cur.i && other.j == cur.j - 1);
+			c += (cur.i - 1 >= 0 && other.i == cur.i - 1 && other.j == cur.j);
+			c += (cur.j + 1 < 4 && other.i == cur.i && other.j == cur.j + 1);
+			c += (cur.i + 1 < 4 && other.i == cur.i + 1 && other.j == cur.j);
+		}
+	}
+	if (c < 6)
+		item->is_valid = 0;
 }
 
-t_item      *create_valid_item(char chr, const char *src)
+t_item			*create_item(char chr, const char *src)
 {
-    t_item  *item;
-    size_t  i;
+	t_item	*item;
 
-    if (!(item = malloc(sizeof(t_item))))
-        return NULL;
-    item->chr = chr;
-    item->posi = 0;
-    item->posj = 0;
-    item->is_valid = (ft_strlen(src) == 20 && ft_strcount(src, '\n') == 4 &&
-                    ft_strcount(src, '#') == 4 && ft_strcount(src, '.') == 12);
-    _set_points(item, _to_matrix(src));
-    _validate_item(item);
-    if (!item->is_valid)
-    {
-        free(item);
-        return (NULL);
-    }
-    return (item);
+	if (!(item = malloc(sizeof(t_item))))
+		return (NULL);
+	item->chr = chr;
+	item->posi = 0;
+	item->posj = 0;
+	item->is_valid = (ft_strlen(src) == 20 && ft_strcount(src, '\n') == 4 &&
+					ft_strcount(src, '#') == 4 && ft_strcount(src, '.') == 12);
+	set_points(item, to_matrix(src));
+	validate_item(item);
+	return (item);
 }
--- src/C/item.h
+++ src/C/item.h
@@ -1,18 +1,36 @@
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   item.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sjacelyn <sjacelyn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/09 09:36:48 by sjacelyn          #+#    #+#             */
+/*   Updated: 2019/03/09 13:13:28 by sjacelyn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-typedef struct  s_point
+#ifndef ITEM_H
+# define ITEM_H
+
+# include <stdlib.h>
+# include "libft/libft.h"
+
+typedef struct	s_point
 {
-    char        i;
-    char        j;
-}               t_point;
+	char		i;
+	char		j;
+}				t_point;
 
-typedef struct  s_item
+typedef struct	s_item
 {
-    short       posi;
-    short       posj;
-    char        chr;
-    char        is_valid;
-    t_point     points[4];
-}               t_item;
+	short		posi;
+	short		posj;
+	char		chr;
+	char		is_valid;
+	t_point		points[4];
+}				t_item;
 
-t_item          *create_item(char chr, const char *src);
+t_item			*create_item(char chr, const char *src);
+
+#endif
